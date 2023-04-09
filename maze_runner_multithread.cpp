2c67a5c0e984ff0c1b1ff8b2827b6432ef2c0d11
matheus cascalho <matheus.santos@enacom.com.br>
2023-04-08 21:25:09 -0300

    busca concorrente com multithread

--- maze_runner_multithread.cpp
+++ maze_runner_multithread.cpp
@@ -7,7 +7,11 @@
 #include <thread>
 #include <chrono>
 #include <algorithm>
-#include <thread>
+#include <mutex>
+
+using namespace std;
+
+mutex m;
 
 // Posição a ser explorada
 struct pos_t
@@ -29,14 +33,14 @@ struct maze_info
 	matrix_size size;
 };
 
-class OpenFileError : public std::exception {
+class OpenFileError : public exception {
 	public:
 		const char* what() const throw() {
 			return "Erro na leitura do arquivo";
 		}
 };
 
-class FinishMaze : public std::exception {
+class FinishMaze : public exception {
 	public:
 		const char* what() const throw() {
 			return "Labirinto finalizado!";
@@ -45,22 +49,22 @@ class FinishMaze : public std::exception {
 
 // Estrutura de dados contendo as próximas
 // posicões a serem exploradas no labirinto
-std::stack<pos_t> valid_positions;
+stack<pos_t> position_buffer;
 
-std::vector<std::vector<char>> global_maze;
+vector<vector<char>> global_maze;
 
 void print_maze()
 {
-	std::cout << "\n====================================================================================\n";
+	cout << "\n====================================================================================\n";
 	for (const auto& linha:global_maze)
 	{
 		for (const auto& caractere : linha)
 		{
-			std::cout << caractere << " ";
+			cout << caractere << " ";
 		}
-		std::cout << "\n";
+		cout << "\n";
 	}
-	std::cout << "\n====================================================================================\n";
+	cout << "\n====================================================================================\n";
 }
 
 maze_info load_maze(const char* file_name)
@@ -68,7 +72,7 @@ maze_info load_maze(const char* file_name)
 	pos_t initial_pos;
 	// Abre o arquivo para leitura 
 	matrix_size matrix;
-	std::ifstream file(file_name);
+	ifstream file(file_name);
 
 	// verifica se o arquivo foi aberto com sucesso
 	if (!file.is_open()){
@@ -76,9 +80,9 @@ maze_info load_maze(const char* file_name)
 	}
 
 	file >> matrix.l >> matrix.c;	
-	std::cout << matrix.l << " " << matrix.c << std::endl;
+	cout << matrix.l << " " << matrix.c << endl;
 
-	std::string line;
+	string line;
 	int i = 0;
 	while(getline(file, line)){
         if(i == 0){
@@ -86,7 +90,7 @@ maze_info load_maze(const char* file_name)
 			continue;
 		}
 		else {
-			std::vector<char> chars(line.begin(), line.end());
+			vector<char> chars(line.begin(), line.end());
 			global_maze.push_back(chars);
 		}
 		
@@ -110,27 +114,27 @@ maze_info load_maze(const char* file_name)
 	info.size = matrix;
 	info.input_maze = initial_pos;
 	info.current_position = initial_pos;
-	valid_positions.push(info.current_position);
 	return info;
 }
 
 
 
-// std::thread create_thread(pos_t next, maze_info info){
+// thread create_thread(pos_t next, maze_info info){
 // 		maze_info next_info;
 		
 // 		next_info.size = info.size;
 // 		next_info.input_maze = info.input_maze;
 // 		next_info.current_position = next;
 
-// 		std::thread t(walk, next_info);
+// 		thread t(walk, next_info);
 // 		return t;
 // }
 
 /* Verifica as posições vizinhas à posição atual em busca de um caminho válido*/
-void update_valid_position(maze_info info){
+vector<pos_t> get_valid_position(maze_info info){
 	char valid_symbol = 'x';
-	// std::vector<std::thread> threads;
+	vector<pos_t> valid_positions;
+	// vector<thread> threads;
 	
 	if(info.current_position.j < info.size.c - 1  && global_maze[info.current_position.i][info.current_position.j+1] == valid_symbol){
 		pos_t next;
@@ -138,51 +142,52 @@ void update_valid_position(maze_info info){
 		next.i = info.current_position.i;
 		next.j = info.current_position.j + 1;
 
-		// std::thread t(create_thread(next, info));
+		// thread t(create_thread(next, info));
   		// threads.push_back(t);
 
-		valid_positions.push(next);
+		valid_positions.push_back(next);
 	}
 	if(info.current_position.j > 0 && global_maze[info.current_position.i][info.current_position.j-1] == valid_symbol){
 		pos_t next;
 		next.i = info.current_position.i;
 		next.j = info.current_position.j - 1;
 		
-		// std::thread t(create_thread(next, info));
+		// thread t(create_thread(next, info));
   		// threads.push_back(t);
 
-		valid_positions.push(next);
+		valid_positions.push_back(next);
 	}
 	if(info.current_position.i < info.size.l - 1 && global_maze[info.current_position.i+1][info.current_position.j] == valid_symbol){
 		pos_t next;
 		next.i = info.current_position.i+1;
 		next.j = info.current_position.j;
 		
-		// std::thread t(create_thread(next, info));
+		// thread t(create_thread(next, info));
   		// threads.push_back(t);
 
-		valid_positions.push(next);
+		valid_positions.push_back(next);
 	}
 	if(info.current_position.i > 0 && global_maze[info.current_position.i-1][info.current_position.j] == valid_symbol){
 		pos_t next;
 		next.i = info.current_position.i-1;
 		next.j = info.current_position.j;
 		
-		// std::thread t(create_thread(next, info));
+		// thread t(create_thread(next, info));
   		// threads.push_back(t);
 
-		valid_positions.push(next);
+		valid_positions.push_back(next);
 	}
+	return valid_positions;
 }
 
 /* Adiciona o simbolo '.' no labirinto*/
 void walk(maze_info& info){
 	char symbol = '.';
 	global_maze[info.current_position.i][info.current_position.j] = symbol;
-	update_valid_position(info=info);
+	
 }
 
-std::vector<std::thread> threads;
+vector<thread> threads;
 
 /* Função Recursiva para caminhar sobre o labirinto
 	caminha:
@@ -209,36 +214,78 @@ std::vector<std::thread> threads;
 // 	next_info.current_position = next;
 // }
 
-void search(maze_info info){
-	
-	info.current_position = valid_positions.top();
-	valid_positions.pop();
-	walk(info);
-}
+// void search(maze_info& info){
+// 	info.current_position = valid_positions.top();
+// 	valid_positions.pop();
+// 	walk(info);
+// }
 
+thread new_search(maze_info info){
+	// m.lock();
+	walk(info);
+	vector<pos_t> valid_positions;
+	valid_positions = get_valid_position(info);
+	if(!valid_positions.empty()){
+		info.current_position = valid_positions.back();
+		valid_positions.pop_back();
+		walk(info);
+		print_maze();
+		valid_positions = get_valid_position(info);
+		for(auto& position: valid_positions){
+			position_buffer.push(position);
+		}
+
+	}
+	// m.unlock();
+}
 
 int main(int argc, char* argv[])
 {
 	// carregar o labirinto com o nome do arquivo recebido como argumento (argv[])
 	try{
 		maze_info info = load_maze("maze2.txt");			
-		if (valid_positions.empty()){
-			throw FinishMaze();
+		// if (valid_positions.empty()){
+		// 	throw FinishMaze();
+		// }
+		// else {
+			// while(!valid_positions.empty()){
+		print_maze();
+		vector<pos_t> valid_positions;
+		valid_positions = get_valid_position(info);
+		while(!valid_positions.empty()){
+			info.current_position = valid_positions.back();
+			valid_positions.pop_back();
+			if(valid_positions.size()>0){
+				for (int i = 0; i < valid_positions.size(); i++){
+					maze_info current_info(info);
+					current_info.current_position = valid_positions.back();
+					valid_positions.pop_back();
+					thread t(new_search, ref(current_info));
+					threads.push_back(move(t));
+					this_thread::sleep_for(chrono::milliseconds(50));
+				}
+			}
+			if(position_buffer.size()>0){
+				for (int i = 0; i < position_buffer.size(); i++){
+					maze_info current_info(info);
+					current_info.current_position = position_buffer.top();
+					position_buffer.pop();
+					thread t(new_search, ref(current_info));
+					threads.push_back(move(t));
+					this_thread::sleep_for(chrono::milliseconds(50));
+				}
+			}
+			walk(info);
+			print_maze();
+			valid_positions = get_valid_position(info);				
+			this_thread::sleep_for(chrono::milliseconds(50));
 		}
-		else {
-			while(!valid_positions.empty()){
-				print_maze();
-				search(info);
-				std::this_thread::sleep_for(std::chrono::milliseconds(50));
-			}			
-		}
-
 		
 		for(auto& thread : threads){
 			thread.join();
 		}
-	} catch (std::exception& e){
-		std::cout << e.what() << std::endl;
+	} catch (exception& e){
+		cout << e.what() << endl;
 		return 1;
 	}
 }
